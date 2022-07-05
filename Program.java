f0a2977df34d137a80476ab5ee2b12894f7ecb36
vinod-bhagat <73749484+vinod-bhagat@users.noreply.github.com>
2022-07-05 15:49:38 +0530

    version-2

--- Program.java
+++ Program.java
@@ -1,5 +1,6 @@
 public class Program{
     public static void main(String[] args){
         System.out.println("Hello SonarQube!!!");
+        System.out.println("First Git and SonarQube project!!!");
     }
 }
